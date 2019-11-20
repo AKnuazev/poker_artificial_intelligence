75e0de1cbe50e420a74f00b93c068b958b66e15f
AKnuazev <2536040@gmail.com>
2019-11-20 23:57:52 +0300

    Edited Graph system

---
+++ source/TestGraph.py
@@ -0,0 +1,12 @@
+from graphviz import Digraph
+dot = Digraph(comment='The Round Table')
+dot.node('A', 'King Arthur')
+dot.node('B', 'Sir Bede the Wise')
+dot.node('L', 'Sir Anton the Brave')
+
+dot.edges(['AB', 'AL'])
+dot.edge('B', 'L', constraint='false')
+
+print(dot.source)
+
+dot.render('test-output/round-table.gv', view=True)
